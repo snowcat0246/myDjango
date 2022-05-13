2fdee93b25778b344a4a4e9887bedad2c7b1ad1e
snowcat0246 <catqqww0246zxc@gmail.com>
2022-05-13 16:58:51 +0800

    modified: settings

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def sayhello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
--- mydjago/urls.py
+++ mydjago/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import sayhello
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sayhello/',sayhello),
     
 ]
